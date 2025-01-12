3d310ba54cd217498ac941d137d3afcfd62501ff
Pranub150 <pranubhandari02@gmail.com>
2025-01-12 10:15:39 +0545

    operator precedence

---
+++ Operator Precedence.py
@@ -0,0 +1,15 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(v + w) * x / y;
+print("Valiue of (v + w) * x / y")
+
+name = "Alex"
+age=0
+if name == "Alex" or name == "John" and age >= 2:
+    print("Hello Welcome")
+else:
+    print("Good bye!")
+    
